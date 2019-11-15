752ff3411de27c3a5a41b3fdf65c60e8194b09dd
Richard Falcema <richard@falcema.com>
2019-11-15 23:06:53 +0900

    save

--- descartia.js
+++ descartia.js
@@ -1,12 +1,14 @@
 (function(){
   console.log('executed');
 
+  var table = document.querySelector('.l-table');
   var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  window.addEventListener(scroll_event,disableScroll,{ passive: false });
-  function disableScroll(e){
+  window.addEventListener('scroll',scrollfunction,{ passive: false });
+  function scrollfunction(e){
     //e.preventDefault();
     var scrolled = window.pageYOffset;
     document.getElementById('scroll-value').innerHTML = scrolled;
+    table.style.transform = 'translateY(-'+scrolled+'px)';
 
   }
 
