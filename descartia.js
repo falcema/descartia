d99a26960b0d66b825c5990573fd4107f8c9985c
Richard Falcema <richard@falcema.com>
2019-12-23 19:42:24 +0900

    remove wheel event and related functions

--- descartia.js
+++ descartia.js
@@ -1,6 +1,4 @@
 (function(){
-  console.log('executed');
-
   var w_width = window.innerWidth;
   var w_height = window.innerHeight;
   var offset;
@@ -13,13 +11,24 @@
   var debounceTimer;
 
 
-  const l_table = document.querySelector('.l-table');
+  const l_table = document.getElementsByClassName('l-table')[0];
   const dummy_scroll = document.getElementsByClassName('dummy-scroll')[0];
   const scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
 
-  descartia_init();
 
-  window.addEventListener('scroll',scrollfunction,{ passive: false });
+  window.onbeforeunload = function(){
+    //window.scrollTo(0,0);
+  };
+  window.onload = function(){
+    //window.scrollTo(0,0);
+    descartia_init();
+    //l_scroll(window.pageYOffset);
+    setTimeout(function(){
+      l_scroll(window.pageYOffset);
+      window.addEventListener('scroll',scrollfunction,{ passive: false });
+    },100);
+  };
+
   function scrollfunction(e){
     //e.preventDefault();
     var scrolled = window.pageYOffset;
@@ -39,6 +48,7 @@
     l_table_height = l_table.clientHeight;
     y_max = l_table_height - w_height;
     offset = 0;
+    console.log('descartia.js executed');
     console.log(y_max);
     dummy_scroll.style.height = l_table_height+'px';
   }
@@ -74,28 +84,14 @@
   };
 
   function wheelScroll(e) {
-    var w_delta = e.detail ? -e.detail * 40 : e.wheelDelta;
-    var sc_delta;
-    sc_delta = Math.abs(w_delta / 1.5);
     timestamp = Date.now();
     timeConstant = 120;
-    if (w_delta > 0) {
-      //target = offset - sc_delta;
-      target = window.pageYOffset;
-      if(target < 0){
-        target = 0;
-      }
-      amplitude = target - offset;
-      window.requestAnimationFrame(autoScroll);
-    } else {
-      //target = offset + sc_delta;
-      target = window.pageYOffset;
-      if(target > y_max){
-        target = y_max;
-      }
-      amplitude = target - offset;
-      window.requestAnimationFrame(autoScroll);
+    target = window.pageYOffset;
+    if(target < 0){
+      target = 0;
     }
+    amplitude = target - offset;
+    window.requestAnimationFrame(autoScroll);
   };
 
 
