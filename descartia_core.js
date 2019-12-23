52c55e212834620d282b6e087bb4e6630d4f1934
Richard Falcema <richard@falcema.com>
2019-12-24 01:43:55 +0900

    add test start / stop button

--- descartia_core.js
+++ descartia_core.js
@@ -99,7 +99,25 @@ var Descatia = (function(){
 })();
 
 (function(){
+
+  var isClicked = false;
+  const ssbutton = document.getElementById('text-button');
+
   window.onload = function(){
     Descatia.start();
   };
+
+  ssbutton.addEventListener('click',function(){
+    if(isClicked){
+      isClicked = false;
+      ssbutton.innerHTML = "Stop";
+      Descatia.start();
+    }
+    else{
+      isClicked = true;
+      ssbutton.innerHTML = "Start";
+      Descatia.stop();
+    }
+  });
+
 })();
