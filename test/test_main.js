b5ec6ae5434002d9564788dc06ede703346ffb66
Richard Falcema <richard@falcema.com>
2019-12-29 06:10:37 +0900

    move to /test add test indicators

--- test/test_main.js
+++ test/test_main.js
@@ -4,10 +4,21 @@
   const ssbutton = document.getElementById('text-button');
   const ssbutton1 = document.getElementById('text-button1');
   const ssbutton2 = document.getElementById('text-button2');
+  const scrollobj = document.getElementsByClassName('d-table')[0];
+  const scrollval = document.getElementById('descartia-value');
+  const observer = new MutationObserver(records => {
+    scrollval.innerHTML = Descartia.currentScrolled();
+  });
 
   window.onload = function(){
     Descartia.setMinWidth(600);
     Descartia.start();
+    observer.observe(scrollobj,{
+      attributes: true,
+      attributeFilter: ['class','style']
+    });
+
+
   };
 
   ssbutton.addEventListener('click',function(){
@@ -21,5 +32,12 @@
   ssbutton2.addEventListener('click',function(){
     Descartia.disable();
   });
+
+  window.addEventListener('scroll',value_indicator);
+  function value_indicator(){
+    var scrolled = window.pageYOffset;
+    document.getElementById('scroll-value').innerHTML = scrolled;
+  }
+
 
 })();
